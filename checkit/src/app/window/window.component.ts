67372676be1e572e79b8f5c57373b492d58eae16
Juliette Cochez <juliette2575@gmail.com>
2022-12-07 16:20:28 +0100

    Affichage premier component

--- checkit/src/app/window/window.component.ts
+++ checkit/src/app/window/window.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-window',
   templateUrl: './window.component.html',
   styleUrls: ['./window.component.css']
 })
-export class WindowComponent {
+export class WindowComponent implements OnInit{
+  title! : string;
+  description! : string;
+  creationDate!: Date;
+  like! : number;
 
+  ngOnInit() {
+    this.title = "Article";
+    this.description = "Fenêtre d'affichage de l'article";
+    this.creationDate = new Date();
+    this.like = 6;
+  }
 }
