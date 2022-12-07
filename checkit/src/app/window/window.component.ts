a8cc2b6c3986d73dffa85d193623f94efa9ba1cf
Juliette Cochez <juliette2575@gmail.com>
2022-12-07 16:42:25 +0100

    Ajout bouton

--- checkit/src/app/window/window.component.ts
+++ checkit/src/app/window/window.component.ts
@@ -19,4 +19,8 @@ export class WindowComponent implements OnInit{
     this.like = 6;
     this.imageUrl = "https://media-exp1.licdn.com/dms/image/C5603AQGsCYZqm5BJwA/profile-displayphoto-shrink_800_800/0/1604946547057?e=2147483647&v=beta&t=7Qg1RvPNXY3lIcV-XDU84IxxC8XGsVvf3jc8fOngnn4"
   }
+
+  onAddLike(){
+    this.like++;
+  }
 }
