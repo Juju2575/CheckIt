2338fc8430be7a30cf457ff25cf3d756d4ec57fd
Juliette Cochez <juliette2575@gmail.com>
2022-12-08 11:47:40 +0100

    Ajout d'un header CheckIt au site

--- checkit/src/app/app.module.ts
+++ checkit/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { WindowComponent } from './window/window.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WindowComponent
+    WindowComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
