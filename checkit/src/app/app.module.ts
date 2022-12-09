93ef4c18af6dd950f543415cc3c5b9d65e6879f9
Juliette Cochez <juliette2575@gmail.com>
2022-12-09 15:06:36 +0100

    Renommage component window en textBox

--- checkit/src/app/app.module.ts
+++ checkit/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { WindowComponent } from './window/window.component';
 import { HeaderComponent } from './header/header.component';
 import { SnapFaceTutorialComponent } from './snap-face-tutorial/snap-face-tutorial.component';
+import { TextBoxComponent } from './text-box/text-box.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WindowComponent,
     HeaderComponent,
-    SnapFaceTutorialComponent
+    SnapFaceTutorialComponent,
+    TextBoxComponent
   ],
   imports: [
     BrowserModule,
