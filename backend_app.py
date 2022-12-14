a83904cfac129b8daf450bfffbbd2d43d0eb913d
unknown <thomas.renaud@student-cs.fr>
2022-12-14 16:34:44 +0100

    adding the scraping of an euronews article

--- backend_app.py
+++ backend_app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+from euronews_scraping import euronews_retrieve_info
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
@@ -22,6 +23,16 @@ def show_website():
         return jsonify(url[0])
     else:
         return jsonify(url[0])
+
+
+@app.route('/articleInfos', methods=["GET"])
+def show_infos():
+    return jsonify(euronews_retrieve_info('https://fr.euronews.com/2022/12/13/conference-de-soutien-a-lukraine-laide-doit-arriver-en-temps-reel-sur-le-terrain'))
+
+    try:
+        return jsonify(euronews_retrieve_info('https://fr.euronews.com/2022/12/13/conference-de-soutien-a-lukraine-laide-doit-arriver-en-temps-reel-sur-le-terrain'))
+    except:
+        return jsonify()
 
 
 app.run()
