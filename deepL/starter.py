c6b957e15b8770785c070d5a143f4625efd5ab24
Juliette Cochez <juliette2575@gmail.com>
2022-12-14 16:47:14 +0100

    Exploration jeu de données Kaggle

---
+++ deepL/starter.py
@@ -0,0 +1,17 @@
+#%% Imports
+
+from mpl_toolkits.mplot3d import Axes3D
+from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt # plotting
+import numpy as np # linear algebra
+import os # accessing directory structure
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+
+#%% Read Data
+
+df = pd.read_csv('datasets/news-article-categories.csv')
+
+#%% Dataframe info
+ 
+df.info
+# %% 
