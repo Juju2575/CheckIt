bf3efd147e85a826baa32af5e79a504978504699
unknown <thomas.renaud@student-cs.fr>
2023-02-08 14:53:54 +0100

    starting the class architecture

---
+++ backend_app/articles.py
@@ -0,0 +1,11 @@
+class Check_It_Article:
+
+    def __init__(self):
+        self.title = ''
+        self.url = ''
+    
+    def set_title(self, title):
+        self.title = title
+    
+    def retrieve_info():
+        return
