ac94a518d768288f2e836e8062737d0522399033
unknown <thomas.renaud@student-cs.fr>
2023-03-02 15:45:02 +0100

    adding the current changes before a bit of refacto

--- checkit/src/app/text-box/text-box.component.ts
+++ checkit/src/app/text-box/text-box.component.ts
@@ -13,43 +13,67 @@ import { ArrayType } from '@angular/compiler';
 export class TextBoxComponent {
   constructor(private httpClient: HttpClient) { }
   value = new Article;
-
-
-  retrieveInfo(text: string) {
-    return this.httpClient.get<Article>('http://127.0.0.1:5000/articleInfos', { headers: { text } }).subscribe(x => {
-      console.log(x);
-      this.value = x;
-      console.log('Hello');
-      var displayBox = document.getElementById("title");
-      if (displayBox != undefined && this.value.title != '') {
-        displayBox.textContent = 'Title : '.concat(this.value.title);
+  isLoaded = false;
+  infoPromise(text: string) {
+    return new Promise((resolve, reject) => {
+      var rep = this.httpClient.get<Article>('http://127.0.0.1:5000/articleInfos', { headers: { text } }).subscribe(x => {
+        console.log(x);
+        this.value = x;
+        this.isLoaded = true;
+      });
+      console.log(this.value)
+      if (rep) {
+        resolve('')
+        return
       }
-      displayBox = document.getElementById("author");
-      if (displayBox != undefined && this.value.author != '') {
-        displayBox.textContent = 'Author : '.concat(this.value.author);
-      }
-      displayBox = document.getElementById("summary");
-      if (displayBox != undefined && this.value.summary != '') {
-        displayBox.textContent = 'Summary : '.concat(this.value.summary);
-      }
-      displayBox = document.getElementById("date");
-      if (displayBox != undefined && this.value.creationDate != '') {
-        displayBox.textContent = 'Article creation date : '.concat(this.value.creationDate);
-      }
-      var ul = document.getElementById("topicsList");
-      var li;
-      var topics = this.value.topics.substring(1, this.value.topics.length - 1).split(",");
-      // console.log(topics);
-      // console.log(typeof topics);
-      // console.log(typeof this.value.topics);
-      if (ul != undefined) {
-        for (let i = 0; i < topics.length; i++) {
-          li = document.createElement("li");
-          li.appendChild(document.createTextNode(topics[i]));
-          ul.appendChild(li);
-        }
-      }
+      reject('');
     });
+  }
+
+  retrievePromise(text: string) {
+    let promise = this.infoPromise(text);
+    promise.then(
+      (n) => {
+        console.log('Hello');
+        this.displayInfo();
+      },
+      (t) => console.log('no info registred'))
+  };
+
+  displayInfo() {
+    var displayBox = document.getElementById("title");
+    if (displayBox != undefined && this.value.title != '') {
+      displayBox.textContent = 'Title : '.concat(this.value.title);
+    }
+    displayBox = document.getElementById("author");
+    if (displayBox != undefined && this.value.author != '') {
+      displayBox.textContent = 'Author : '.concat(this.value.author);
+    }
+    displayBox = document.getElementById("summary");
+    if (displayBox != undefined && this.value.summary != '') {
+      displayBox.textContent = 'Summary : '.concat(this.value.summary);
+    }
+    displayBox = document.getElementById("date");
+    if (displayBox != undefined && this.value.creationDate != '') {
+      displayBox.textContent = 'Article creation date : '.concat(this.value.creationDate);
+    }
+    var ul = document.getElementById("topicsList");
+    var li;
+    var topics = this.value.topics.substring(1, this.value.topics.length - 1).split(",");
+    // console.log(topics);
+    // console.log(typeof topics);
+    // console.log(typeof this.value.topics);
+    if (ul != undefined) {
+      while (ul.firstChild) {
+        ul.removeChild(ul.firstChild)
+      }
+      for (let i = 0; i < topics.length; i++) {
+        li = document.createElement("li");
+        li.appendChild(document.createTextNode(topics[i]));
+        ul.appendChild(li);
+      }
+    }
+    ;
 
 
   }
